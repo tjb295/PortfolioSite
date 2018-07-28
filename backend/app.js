4cd88416f03d2c9f95aa2371b688f6a094404c39
Thomas Back <tjb295@nau.edu>
2018-07-27 18:09:35 -0700

    Boilerplate for node server w/ express

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+//thi will hold express app setup
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+module.exports = app;
